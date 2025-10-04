1fd91dfb54e46dc36bc13a295a4f96215efba0f6
Brian Bosh <bbosh@alternateinterior.com>
2025-10-04 17:07:28 -0600

    separate batter from runner actions

--- src/entity/action.ts
+++ src/entity/action.ts
@@ -1,5 +1,6 @@
-export const OffensiveActions = ["swing", "take", "bunt", "steal", "advance", "stay"] as const;
+export const OffensiveBaseActions = ["steal", "advance", "stay"] as const;
+export const OffensiveHitterActions = ["swing", "take", "bunt"] as const;
 export const DefensiveActions = ["catch", "check", "tag", "force", "throw"] as const;
-export type OffensiveAction = typeof OffensiveActions[number];
+export type OffensiveAction = typeof OffensiveBaseActions[number] | typeof OffensiveHitterActions[number];
 export type DefensiveAction = typeof DefensiveActions[number];
 export type Action = OffensiveAction | DefensiveAction;
