3a7c3b3e103b02e52023eed66c69225cdf56c43f
Brian Bosh <bbosh@alternateinterior.com>
2025-10-04 17:45:44 -0600

    Test pitcher reliever logic

--- src/entity/game-team.spec.ts
+++ src/entity/game-team.spec.ts
@@ -1,7 +1,45 @@
 import { describe, expect, it } from "vitest";
+import { GameTeam } from "./game-team";
+import { Team } from "./team";
+import { baltimoreFederals } from "../test/fixture/team/baltimore-federals";
+import { Lineup } from "./lineup";
+import { TeamPlayer } from "./team-player";
+import { Player } from "./player";
+import { ActiveRoster } from "./roster";
 
 describe("game-team", () => {
-  it("is a placeholder", () => {
-    expect(true).toBe(true);
+  it("is should maintain a proper bullpen", () => {
+    const team = new Team({
+      city: "F",
+      name: "F",
+      awayColor: baltimoreFederals.awayColor,
+      homeColor: baltimoreFederals.homeColor,
+      players: [
+        ...baltimoreFederals.players
+      ]
+    });
+    const gameTeam = new GameTeam(
+      team,
+      new Lineup(
+        team,
+        team.players.find(p => p.position === "P")!,
+        false,
+      )
+    );
+    expect(gameTeam.bullPen().length).toBe(0);
+    const original = gameTeam.team.players.slice();
+    gameTeam.team.players.push(
+      new TeamPlayer({
+        player: new Player({ firstName: "P", lastName: "2", attributes: [] }),
+        number: 99,
+        activeFrom: new Date(2020, 0, 1), 
+        position: "P"
+      })
+    );
+    expect(gameTeam.bullPen().length).toBe(1);
+    const reliever = gameTeam.bullPen()[0];
+    expect(gameTeam.relieve(gameTeam.lineUp.positions.P, reliever)).toBe(void 0);
+    expect(() => gameTeam.relieve(gameTeam.lineUp.positions.P, reliever)).toThrow("Cannot relieve with player 2 (#99, P), not in bullpen");
+    gameTeam.team.players = original as unknown as ActiveRoster;
   });
 })
--- src/entity/game-team.ts
+++ src/entity/game-team.ts
@@ -1,11 +1,23 @@
 import { Lineup } from "./lineup";
 import { Team } from "./team";
+import { TeamPlayer } from "./team-player";
 
 export class GameTeam {
   team: Team;
   lineUp: Lineup;
+  relieved: TeamPlayer[] = [];
   constructor (team: Team, lineUp: Lineup) {
     this.team = team;
     this.lineUp = lineUp;
   } 
+  bullPen(): TeamPlayer[] {
+    return this.team.players.filter(p => p.position === "P" && p !== this.lineUp.positions["P"] && !this.relieved.includes(p));
+  }
+  relieve(fatigued: TeamPlayer, fresh: TeamPlayer): void {
+    if(!this.bullPen().includes(fresh)) {
+      throw new Error(`Cannot relieve with player ${fresh}, not in bullpen`);
+    }
+    this.relieved.push(fatigued);
+    this.lineUp.positions[fatigued.position] = fresh;
+  }
 }
